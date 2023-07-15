20a9ea6cbf314a2329eb813b10cbbcd01b751aa4
SuperFeliz <linuxeopoder@gmail.com>
2023-07-14 22:11:57 -0300

    Exibir as categorias no HTML

--- perfil/views.py
+++ perfil/views.py
@@ -11,12 +11,13 @@ def home(request):
 
 def gerenciar(request):
     contas = Conta.objects.all()
+    categorias = Categoria.objects.all()
     #total_contas = contas.aggregate(Sum('valor'))['valor__sum']
     total_contas = 0
 
     for conta in contas:
         total_contas += conta.valor
-    return render(request, 'gerenciar.html', {'contas': contas, 'total_contas': total_contas})
+    return render(request, 'gerenciar.html', {'contas': contas, 'total_contas': total_contas, 'categorias': categorias})
 
 def cadastrar_banco(request):
     apelido = request.POST.get('apelido')
@@ -28,6 +29,8 @@ def cadastrar_banco(request):
     if len(apelido.strip()) == 0 or len(valor.strip()) == 0:
         messages.add_message(request, constants.ERROR, 'Preencha todos os campos')
         return redirect('/perfil/gerenciar/')
+    
+    #TODO: Fazer mais validações
     
     conta = Conta(
         apelido = apelido,
@@ -54,6 +57,9 @@ def deletar_banco(request, id):
 def cadastrar_categoria(request):
     nome = request.POST.get('categoria')
     essencial = bool(request.POST.get('essencial'))
+
+
+    #TODO: Fazer validações 
 
     categoria = Categoria(
         categoria=nome,
